01eefe78ba5aa7b12330a74d9316875531c1be9d
YuLian <yulian9688@gmail.com>
2022-06-15 22:03:49 +0900

    [Fin]sta7

--- src/App.js
+++ src/App.js
@@ -12,19 +12,38 @@ export const App = () => {
     'https://images.dog.ceo/breeds/pinscher-miniature/n02107312_471.jpg',
   )
 
+  const getRandomDog = () => {
+    let response = fetch('https://dog.ceo/api/breeds/image/random')
+    response
+      .then(response => response.json())
+      .then(response => {
+        if (response.status !== 'success') {
+          throw 'api unsucess'
+        } else {
+          setDogUrl(response.message)
+        }
+      })
+  }
   return (
     <div>
       <header>
         <h1>Dogアプリ</h1>
-        <p>犬の画像を表示するサイトです。</p>
-        <img src={dogUrl} alt="dog" /><br/>
-        <button
-          onClick={() => {
-            setDogUrl( `https://images.dog.ceo/breeds/shiba/shiba-9.jpg` )
-          }}
-        >
-          change
-        </button>
+        <div className="show-container">
+          <div className="descripe-container">
+            <p className="descripe">犬の画像を表示するサイトです。</p>
+          </div>
+          <div className="image-container">
+            <img className="dog-img" src={dogUrl} alt="dog" />
+            <br />
+            <button
+              onClick={() => {
+                getRandomDog()
+              } }
+              className="update-button">
+              更新
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   )
