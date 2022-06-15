ba292d9083fb62258bcf7dedbc7d43db1e2f40e6
YuLian <yulian9688@gmail.com>
2022-06-15 20:37:42 +0900

    [Fin]sta5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/pinscher-miniature/n02107312_471.jpg',
+  )
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h1>Dogアプリ</h1>
+        <p>犬の画像を表示するサイトです。</p>
+        <img src={dogUrl} alt="dog" /><br/>
+        <button
+          onClick={() => {
+            setDogUrl( `https://images.dog.ceo/breeds/shiba/shiba-9.jpg` )
+          }}
+        >
+          change
+        </button>
+      </header>
     </div>
   )
 }
